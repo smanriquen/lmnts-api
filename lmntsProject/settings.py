20377fc4d3965270ba37c19b858921446bd70689
Soleyda Manrique Naranjo <Soleyda Manrique Naranjo>
2016-01-13 22:02:32 -0500

    Add static files configuration

--- lmntsProject/settings.py
+++ lmntsProject/settings.py
@@ -128,19 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'C:/Users/Soleyda/Desktop/lmntsProject/static'
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, '../lmntsProject/static'),
 )
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
