53294cc656c1e50de565079cd30f89009cb574e5
Soleyda Manrique Naranjo <Soleyda Manrique Naranjo>
2016-01-13 22:05:02 -0500

    Add static files configuration

--- lmntsProject/settings.py
+++ lmntsProject/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../lmntsProject/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 # List of finder classes that know how to find static files in
 # various locations.
