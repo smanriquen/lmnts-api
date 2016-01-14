8253762a37c9c7edf000fc7ef2895a0fd88d4b1a
Soleyda Manrique Naranjo <Soleyda Manrique Naranjo>
2016-01-13 21:42:15 -0500

    Add static files configuration

--- lmntsProject/settings.py
+++ lmntsProject/settings.py
@@ -128,7 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 if os.environ.get('HEROKU'):  # heroku config:set HEROKU=1
